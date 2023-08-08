610712db89a98c65e2aa3d4a8385ba54b7c5e832
MinY-Lee <leemy08@naver.com>
2023-08-08 13:35:11 +0900

    백준 1158 요세푸스 문제 주석추가

--- 큐/Algo_BJ1158_요세푸스문제.java
+++ 큐/Algo_BJ1158_요세푸스문제.java
@@ -17,13 +17,16 @@ public class Algo_BJ1158_요세푸스문제 {
         for(int i=1;i<=num;i++){
             q.add(i);
         }
+        // 큐가 빌 때 까지 뽑아주기
         while(!q.isEmpty()){
             count++;
+            // 빼야 할 수면 빼주기
             if(count==end){
                 st.append(q.poll());
                 st.append(", ");
                 count = 0;
             } else {
+                // 아니라면 다시 큐에 넣어주기
                 q.add(q.poll());
             }
         }
